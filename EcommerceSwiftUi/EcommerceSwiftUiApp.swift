75cbdd9a1c545e88d7a182947de5d76103d6ec5e
Karim Hamed <karimhamed947@gmail.com>
2025-06-12 16:08:49 +0300

    Initial Commit

---
+++ EcommerceSwiftUi/EcommerceSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceSwiftUiApp.swift
+//  EcommerceSwiftUi
+//
+//  Created by Karim Hamed  on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
